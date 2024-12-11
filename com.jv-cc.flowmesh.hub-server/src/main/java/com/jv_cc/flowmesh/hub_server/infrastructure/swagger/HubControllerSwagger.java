f7c4c387cbca993f0ab1e1fec4f4196bab4a47ad
jihyeChu <09strawberry@naver.com>
2024-12-11 23:39:08 +0900

    [#5] feat: 허브 생성 기능 스웨거 적용

--- com.jv-cc.flowmesh.hub-server/src/main/java/com/jv_cc/flowmesh/hub_server/infrastructure/swagger/HubControllerSwagger.java
+++ com.jv-cc.flowmesh.hub-server/src/main/java/com/jv_cc/flowmesh/hub_server/infrastructure/swagger/HubControllerSwagger.java
@@ -1,18 +1,17 @@
 package com.jv_cc.flowmesh.hub_server.infrastructure.swagger;
 
-import com.jv_cc.flowmesh.hub_server.application.dto.ResDTO;
+import com.jv_cc.flowmesh.hub_server.application.dto.HubDTO;
 import com.jv_cc.flowmesh.hub_server.presentation.request.ReqHubPostDTO;
+import com.jv_cc.flowmesh.hub_server.presentation.response.ResDTO;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
-import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestHeader;
 
 @Tag(name = "Hub", description = "허브 생성, 검색, 수정, 삭제 관련 사용자 API")
 public interface HubControllerSwagger {
@@ -23,6 +22,6 @@ public interface HubControllerSwagger {
             @ApiResponse(responseCode = "400", description = "허브 생성 실패.", content = @Content(schema = @Schema(implementation = ResDTO.class)))
     })
     @PostMapping("/api/hub")
-    ResponseEntity<ResDTO<Long>> createHub(@RequestBody ReqHubPostDTO dto);
+    ResponseEntity<ResDTO<HubDTO>> createHub(@RequestBody ReqHubPostDTO dto);
 
 }
