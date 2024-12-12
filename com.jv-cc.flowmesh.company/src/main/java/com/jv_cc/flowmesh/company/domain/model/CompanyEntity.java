128c08fa2ee76c9d0d90f36dd9f24eabe99665fb
jihyeChu <09strawberry@naver.com>
2024-12-12 23:43:20 +0900

    [#23] chore: company init

--- com.jv-cc.flowmesh.company/src/main/java/com/jv_cc/flowmesh/company/domain/model/CompanyEntity.java
+++ com.jv-cc.flowmesh.company/src/main/java/com/jv_cc/flowmesh/company/domain/model/CompanyEntity.java
@@ -1,10 +1,7 @@
-package com.jv_cc.flowmesh.hub_server.domain.model;
+package com.jv_cc.flowmesh.company.domain.model;
 
 import io.hypersistence.utils.hibernate.id.Tsid;
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import jakarta.validation.constraints.NotNull;
 import lombok.*;
 import org.hibernate.annotations.ColumnDefault;
@@ -18,24 +15,25 @@ import java.time.LocalDateTime;
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-@Table(name = "p_hub")
-public class HubEntity {
+@Table(name = "p_company")
+public class CompanyEntity {
 
     @Id @Tsid
-    @Column(name = "hub_id")
+    @Column(name = "company_id")
     private Long id;
+
+    @Column(name = "hub_id", nullable = false)
+    private Long hubId;
 
     @Column(name = "name", nullable = false)
     private String name;
 
+    @Enumerated(EnumType.STRING)
+    @Column(name = "type", nullable = false)
+    private CompanyType type;
+
     @Column(name = "address", nullable = false)
     private String address;
-
-    @Column(name = "latitude", nullable = false)
-    private Double latitude;
-
-    @Column(name = "longitude", nullable = false)
-    private Double longitude;
 
     @CreationTimestamp
     @Column(name = "created_at", nullable = false, updatable = false)
@@ -62,28 +60,20 @@ public class HubEntity {
     @Column(name = "is_deleted")
     private boolean isDeleted = false;
 
-    public void update(String name, String address, double latitude, double longitude) {
-        this.name = name;
-        this.address = address;
-        this.latitude = latitude;
-        this.longitude = longitude;
-    }
-
     /*
     * TODO: deletedBy = userId 추가
-    * */
+    */
     public void markAsDelete() {
         this.isDeleted = true;
         this.deletedAt = LocalDateTime.now();
     }
 
-    public HubEntity(String name, String address, Double latitude, Double longitude, Long userId) {
+    public CompanyEntity(Long hubId, String name, CompanyType type, String address, Long userId) {
+        this.hubId = hubId;
         this.name = name;
+        this.type = type;
         this.address = address;
-        this.latitude = latitude;
-        this.longitude = longitude;
         this.createdBy = userId;
         this.updatedBy = userId;
     }
-
 }
