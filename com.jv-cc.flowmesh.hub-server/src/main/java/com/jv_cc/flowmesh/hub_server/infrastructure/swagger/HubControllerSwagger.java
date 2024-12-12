5db24d50c46e0c94f2f352a1b7864957fae7ba42
jihyeChu <09strawberry@naver.com>
2024-12-12 19:00:04 +0900

    [#16] feat: 허브 삭제 기능 스웨거 적용

--- com.jv-cc.flowmesh.hub-server/src/main/java/com/jv_cc/flowmesh/hub_server/infrastructure/swagger/HubControllerSwagger.java
+++ com.jv-cc.flowmesh.hub-server/src/main/java/com/jv_cc/flowmesh/hub_server/infrastructure/swagger/HubControllerSwagger.java
@@ -1,6 +1,5 @@
 package com.jv_cc.flowmesh.hub_server.infrastructure.swagger;
 
-import com.jv_cc.flowmesh.hub_server.application.dto.HubDTO;
 import com.jv_cc.flowmesh.hub_server.presentation.request.ReqHubPostDTO;
 import com.jv_cc.flowmesh.hub_server.presentation.response.ResDTO;
 import com.jv_cc.flowmesh.hub_server.presentation.response.ResHubDTO;
@@ -16,7 +15,7 @@ import org.springframework.web.bind.annotation.*;
 @Tag(name = "Hub", description = "허브 생성, 검색, 수정, 삭제 관련 사용자 API")
 public interface HubControllerSwagger {
 
-    @Operation(summary = "허브 생성", description = "사용자 ID를 통해 메뉴를 생성하는 API 입니다.")
+    @Operation(summary = "허브 생성", description = "사용자 ID를 통해 허브를 생성하는 API 입니다.")
     @ApiResponses(value = {
             @ApiResponse(responseCode = "201", description = "허브 생성 성공", content = @Content(schema = @Schema(implementation = ResDTO.class))),
             @ApiResponse(responseCode = "400", description = "허브 생성 실패.", content = @Content(schema = @Schema(implementation = ResDTO.class)))
@@ -24,17 +23,17 @@ public interface HubControllerSwagger {
     @PostMapping("/api/hub")
     ResponseEntity<ResDTO<ResHubDTO>> createHub(@RequestBody ReqHubPostDTO dto);
 
-    @Operation(summary = "허브 수정", description = "사용자 ID를 통해 메뉴를 수정하는 API 입니다.")
+    @Operation(summary = "허브 수정", description = "사용자 ID를 통해 허브를 수정하는 API 입니다.")
     @ApiResponses(value = {
-            @ApiResponse(responseCode = "200", description = "허브 수정 성공", content = @Content(schema = @Schema(implementation = ResDTO.class))),
+            @ApiResponse(responseCode = "201", description = "허브 수정 성공", content = @Content(schema = @Schema(implementation = ResDTO.class))),
             @ApiResponse(responseCode = "400", description = "허브 수정 실패.", content = @Content(schema = @Schema(implementation = ResDTO.class)))
     })
     @PatchMapping("/api/hub/{hubId}")
     ResponseEntity<ResDTO<ResHubDTO>> modifyHub(@PathVariable Long hubId, @RequestBody ReqHubPostDTO dto);
 
-    @Operation(summary = "허브 삭제", description = "사용자 ID를 통해 메뉴를 삭제하는 API 입니다.")
+    @Operation(summary = "허브 삭제", description = "사용자 ID를 통해 허브를 삭제하는 API 입니다.")
     @ApiResponses(value = {
-            @ApiResponse(responseCode = "200", description = "허브 삭제 성공", content = @Content(schema = @Schema(implementation = ResDTO.class))),
+            @ApiResponse(responseCode = "201", description = "허브 삭제 성공", content = @Content(schema = @Schema(implementation = ResDTO.class))),
             @ApiResponse(responseCode = "400", description = "허브 삭제 실패.", content = @Content(schema = @Schema(implementation = ResDTO.class)))
     })
     @DeleteMapping("/api/hub/{hubId}")
