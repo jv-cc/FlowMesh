dc8ad8a6bb331ba26925e55ce39f7b54eecf0bf2
Yeongho Kim <yeongho52@naver.com>
2024-12-12 10:44:04 +0900

    pull develop

---
+++ com.jv-cc.flowmesh.deliverymanager/src/test/java/com/jv_cc/flowmesh/deliverymanager/DeliverymanagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jv_cc.flowmesh.deliverymanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeliverymanagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
