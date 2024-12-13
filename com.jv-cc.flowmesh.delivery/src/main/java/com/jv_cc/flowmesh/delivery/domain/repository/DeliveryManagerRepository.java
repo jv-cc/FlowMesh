1eb5c9d3c4eca59fc23f33b1c64d93b33abb3151
Yeongho Kim <yeongho52@naver.com>
2024-12-13 17:32:40 +0900

    [#8] Delivery 서비스 환경 초기 설정 완료

---
+++ com.jv-cc.flowmesh.delivery/src/main/java/com/jv_cc/flowmesh/delivery/domain/repository/DeliveryManagerRepository.java
@@ -0,0 +1,8 @@
+package com.jv_cc.flowmesh.delivery.domain.repository;
+
+
+import com.jv_cc.flowmesh.delivery.domain.model.DeliveryManagerEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface DeliveryManagerRepository extends JpaRepository<DeliveryManagerEntity, Long> {
+}
---
+++ com.jv-cc.flowmesh.delivery/src/main/java/com/jv_cc/flowmesh/delivery/presentation/response/DeliveryManagerResponseDTO.java
@@ -0,0 +1,10 @@
+package com.jv_cc.flowmesh.delivery.presentation.response;
+
+import com.jv_cc.flowmesh.delivery.application.dto.DeliveryManagerDTO;
+
+public class DeliveryManagerResponseDTO {
+
+    public DeliveryManagerResponseDTO(DeliveryManagerDTO deliveryManagerDTO) {
+
+    }
+}
