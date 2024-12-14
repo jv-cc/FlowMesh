a7213631cb1f395c51e8ece803a66d6aec2381d7
jihyeChu <09strawberry@naver.com>
2024-12-14 13:22:25 +0900

    [#49] feat: 상품 에러 익셉션 추가

--- com.jv-cc.flowmesh.product/src/main/java/com/jv_cc/flowmesh/product/application/exception/Error.java
+++ com.jv-cc.flowmesh.product/src/main/java/com/jv_cc/flowmesh/product/application/exception/Error.java
@@ -12,6 +12,7 @@ public enum Error {
 
     NOT_FOUND_COMPANY(5000, "해당 업체가 존재하지 않습니다."),
 
+    NOT_FOUND_PRODUCT(6000, "해당 상품이 존재하지 않습니다."),
     DUPLICATE_PRODUCT_NAME(6100, "중복된 상품 이름이 존재합니다."),
 
     INTERNAL_SERVER_ERROR(9999, "서버 오류입니다.");
