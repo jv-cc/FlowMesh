5c5b802e8ce88cfd6b1348d4accecc0669906935
jihyeChu <09strawberry@naver.com>
2024-12-14 11:53:24 +0900

    [#49] feat: 상품 @EnableFeignClients 추가

--- com.jv-cc.flowmesh.product/src/main/java/com/jv_cc/flowmesh/product/ProductApplication.java
+++ com.jv-cc.flowmesh.product/src/main/java/com/jv_cc/flowmesh/product/ProductApplication.java
@@ -2,7 +2,9 @@ package com.jv_cc.flowmesh.product;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
+@EnableFeignClients
 @SpringBootApplication
 public class ProductApplication {
 
