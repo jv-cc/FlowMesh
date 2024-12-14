ba516d72627af4cc2c09464ca737733a54855e20
jihyeChu <09strawberry@naver.com>
2024-12-14 12:54:11 +0900

    [#49] feat: 업체 관련 에러 익셉션 추가

--- com.jv-cc.flowmesh.product/src/main/java/com/jv_cc/flowmesh/product/application/exception/CommonExceptionHandler.java
+++ com.jv-cc.flowmesh.product/src/main/java/com/jv_cc/flowmesh/product/application/exception/CommonExceptionHandler.java
@@ -1,5 +1,7 @@
 package com.jv_cc.flowmesh.product.application.exception;
 
+import com.jv_cc.flowmesh.product.application.exception.company.CompanyException;
+import com.jv_cc.flowmesh.product.application.exception.hub.HubException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -10,6 +12,32 @@ import java.util.Map;
 @RequiredArgsConstructor
 @RestControllerAdvice
 public class CommonExceptionHandler {
+
+    @ExceptionHandler(HubException.class)
+    public ResponseEntity<Object> HubExceptionHandler(HubException e) {
+
+        Error error = e.getError();
+
+        return ResponseEntity
+                .status(e.getHttpStatus())
+                .body(Map.of(
+                        "code", error.getCode(),
+                        "message", error.getMessage()
+                ));
+    }
+
+    @ExceptionHandler(CompanyException.class)
+    public ResponseEntity<Object> CompanyExceptionHandler(CompanyException e) {
+
+        Error error = e.getError();
+
+        return ResponseEntity
+                .status(e.getHttpStatus())
+                .body(Map.of(
+                        "code", error.getCode(),
+                        "message", error.getMessage()
+                ));
+    }
 
     @ExceptionHandler(ProductException.class)
     public ResponseEntity<Object> ProductExceptionHandler(ProductException e) {
--- com.jv-cc.flowmesh.product/src/main/java/com/jv_cc/flowmesh/product/application/exception/Error.java
+++ com.jv-cc.flowmesh.product/src/main/java/com/jv_cc/flowmesh/product/application/exception/Error.java
@@ -8,6 +8,12 @@ import lombok.Getter;
 @AllArgsConstructor
 public enum Error {
 
+    NOT_FOUND_HUB(4000, "해당 허브가 존재하지 않습니다."),
+
+    NOT_FOUND_COMPANY(5000, "해당 업체가 존재하지 않습니다."),
+
+    DUPLICATE_PRODUCT_NAME(6100, "중복된 상품 이름이 존재합니다."),
+
     INTERNAL_SERVER_ERROR(9999, "서버 오류입니다.");
 
     final Integer code;
