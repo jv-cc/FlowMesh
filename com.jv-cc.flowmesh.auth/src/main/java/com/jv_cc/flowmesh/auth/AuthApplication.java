40273b578c58a015881beb1630bb190cc82d5350
jihyeChu <09strawberry@naver.com>
2024-12-15 13:29:17 +0900

    [#56] chore: auth - openfeign 의존성 추가

--- com.jv-cc.flowmesh.auth/src/main/java/com/jv_cc/flowmesh/auth/AuthApplication.java
+++ com.jv-cc.flowmesh.auth/src/main/java/com/jv_cc/flowmesh/auth/AuthApplication.java
@@ -3,6 +3,7 @@ package com.jv_cc.flowmesh.auth;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@EnableFeignClients
 @SpringBootApplication
 public class AuthApplication {
 
