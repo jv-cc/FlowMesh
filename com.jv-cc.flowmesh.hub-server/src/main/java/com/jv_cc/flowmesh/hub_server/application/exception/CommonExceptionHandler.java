6d959f4f6b724a68a566f4513523980d8dea8ff0
jihyeChu <09strawberry@naver.com>
2024-12-16 20:01:43 +0900

    [#56] fix: @RestControllerAdvice 와 swagger 충돌 해결
    - @Hidden 을 통해 Swagger 생성 시 ExceptionHandler 를 제외하고 생성되게하여 충돌을 예방합니다.

--- com.jv-cc.flowmesh.hub-server/src/main/java/com/jv_cc/flowmesh/hub_server/application/exception/CommonExceptionHandler.java
+++ com.jv-cc.flowmesh.hub-server/src/main/java/com/jv_cc/flowmesh/hub_server/application/exception/CommonExceptionHandler.java
@@ -1,17 +1,20 @@
 package com.jv_cc.flowmesh.hub_server.application.exception;
 
-import lombok.RequiredArgsConstructor;
+import io.swagger.v3.oas.annotations.Hidden;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import java.util.Map;
 
-@RequiredArgsConstructor
+@Hidden
 @RestControllerAdvice
 public class CommonExceptionHandler {
 
     @ExceptionHandler(HubException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
     public ResponseEntity<Object> HubExceptionHandler(HubException e) {
 
         Error error = e.getError();
