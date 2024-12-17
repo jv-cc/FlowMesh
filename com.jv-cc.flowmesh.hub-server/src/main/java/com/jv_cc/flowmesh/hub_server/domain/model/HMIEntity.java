770732bde748deed0a1a5a1e5ca4b01c67c2fdf0
jihyeChu <09strawberry@naver.com>
2024-12-17 12:16:02 +0900

    [#64] feat: 허브간 이동 정보 엔티티 생성

---
+++ com.jv-cc.flowmesh.hub-server/src/main/java/com/jv_cc/flowmesh/hub_server/domain/model/HMIEntity.java
@@ -0,0 +1,71 @@
+package com.jv_cc.flowmesh.hub_server.domain.model;
+
+import io.hypersistence.utils.hibernate.id.Tsid;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import lombok.*;
+import org.hibernate.annotations.ColumnDefault;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Entity
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "p_hmi", uniqueConstraints = @UniqueConstraint(columnNames = {"start_hub_id", "end_hub_id"}))
+public class HMIEntity {
+
+    @Id
+    @Tsid
+    @Column(name = "hmi_id")
+    private Long hmiId;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "start_hub_id", referencedColumnName = "hub_id")
+    private HubEntity startHubId;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "end_hub_id", referencedColumnName = "hub_id")
+    private HubEntity endHubId;
+
+    @Column(name = "duration", nullable = false)
+    private LocalDateTime duration;
+
+    @Column(name = "distance", nullable = false)
+    private double distance;
+
+    @CreationTimestamp
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    @Column(name = "created_by", updatable = false)
+    private Long createdBy;
+
+    @UpdateTimestamp
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+
+    @Column(name = "updated_by")
+    private Long updatedBy;
+
+    @Column(name = "deleted_at")
+    private LocalDateTime deletedAt;
+
+    @Column(name = "deleted_by")
+    private Long deletedBy;
+
+    @NotNull
+    @ColumnDefault("false")
+    @Column(name = "is_deleted")
+    private boolean isDeleted = false;
+
+    public void markAsDelete(Long userId) {
+        this.isDeleted = true;
+        this.deletedAt = LocalDateTime.now();
+        this.deletedBy = userId;
+    }
+
+}
