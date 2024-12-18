bfb7b5ff28a9798d0be1f5094fea0a2c17dbe864
Yeongho Kim <yeongho52@naver.com>
2024-12-18 09:16:26 +0900

    [#54] Delivery swagger 수정

--- com.jv-cc.flowmesh.deliveryManager/src/main/java/com/jv_cc/flowmesh/deliverymanager/application/exception/CommonExceptionHandler.java
+++ com.jv-cc.flowmesh.deliveryManager/src/main/java/com/jv_cc/flowmesh/deliverymanager/application/exception/CommonExceptionHandler.java
@@ -1,5 +1,6 @@
 package com.jv_cc.flowmesh.deliverymanager.application.exception;
 
+import io.swagger.v3.oas.annotations.Hidden;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -7,6 +8,7 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import java.util.Map;
 
+@Hidden
 @RestControllerAdvice
 @RequiredArgsConstructor
 public class CommonExceptionHandler {
